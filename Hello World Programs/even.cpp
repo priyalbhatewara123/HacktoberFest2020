9a2ac684e8e69713a25b592cad47954b5f50b5e9
Laiba-Nasir <2018cs43@student.uet.edu.pk>
2020-10-30 20:21:58 +0500

    even number function

---
+++ Hello World Programs/even.cpp
@@ -0,0 +1,15 @@
+/*This is cpp program to check even number*/
+#include <iostream>
+using namespace std;
+int main()
+{int n;
+   cout << "Enter a Number" << endl;
+   cin>>n;
+   if(n%2==0)
+   cout<<" the given number is even"<<endl;
+   else
+   {
+      cout<<"the fiven number is odd"<<endl;
+   }
+ return 0;  
+}
